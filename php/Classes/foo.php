d4ad6cdfdfa609f7c4775df0b6b447d426cd444f
Stephen Pelot <spelot@cnm.edu>
2018-10-25 16:23:16 -0600

    new commit

--- php/Classes/foo.php
+++ php/Classes/foo.php
@@ -1,10 +1,41 @@
 <?php
 
+namespace spelot\datadesign;
+require_once(Classes(__DIR__, 2) . "/vendor/autoload.php");
+
+use Ramsey\Uuid\Uuid;
+
+
 class Post {
 	private $postId;
+	/**
+	 * this is the primary key for post
+	 * @var Uuid $postId;
+	 */
 	private $postUserId;
+	/**
+	 * foreign key of user that made this post
+	 * @var UUid $postUserId;
+	 */
 	private $postContent;
+	/**
+	 * actual content of the post
+	 * @var string $postContent;
+	 */
 	private $postDate;
+
+	/**
+	 * constructor for this Post
+	 *
+	 * @param string|Uuid $newPostId id of this Post or null if a new Post
+	 * @param string|Uuid $newPostUserId id of the User that made this Post
+	 * @param string $newPostContent string containing actual post data
+	 * @param \DateTime|string|null $newPostDate date and time Post was sent or null if set to current date and time
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 **/
 
 	public function __construct($newPostId, $newPostUserId, string $newPostContent, $newPostDate = null) {
 		try {
@@ -12,9 +43,9 @@ class Post {
 			$this->setpostUserId($newPostUserId);
 			$this->setPostContent($newPostContent);
 			$this->setPostDate($newPostDate);
-		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError | \$exception){
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptiontype = get_class($exception);
-			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		throw(new $exception($exception->getMessage(), 0, $exception));
 
 		}
 	}
@@ -46,9 +77,8 @@ function setPostId($newPostId): void {
 	try {
 		$uuid = self::validateUuid($newPostId);
 	}
-	{
-	catch
-		(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception)
+
+	catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 }
 $this->postId = $uuid;
 }
@@ -78,11 +108,10 @@ function setPostUserId($newPostUserId): void {
 		$uuid = self::validateUuid($newPostUserId);
 	} catch(\InvalidArgumentException |\ RangeException |\Exception |\TypeError $exception) {
 		$exceptionType = get_class($exception);
-		throw (new$exceptionType($exception->getMessage(), 0, $exception));
+		throw (new$exceptionType($exception->getMessage(), 0, $exception)); }
 		// convert and store the user id
 		$this->postUserId = $uuid;
 	}
-}
 
 /**
  * accessor method for post content
@@ -114,11 +143,12 @@ function setPostContent(string $newPostContent): void {
 	}
 	//verify the post content will fit in the database
 	if(strlen($newPostContent) >= 2000) {
-		throw(new \RangeException("post content is too large"))
+		throw(new \RangeException("post content is too large"));
 	}
 
 	//store the post content
 	$this->postContent = $newPostContent;
+}
 
 	/**
 	 * accessor method for post date
@@ -143,11 +173,11 @@ function setPostContent(string $newPostContent): void {
 	function setPostDate($newPostDate = null): void {
 		// base case: if the date is null, use the current date and time
 		if($newPostDate === null) {
-			$this->postDate = new /DateTime();
+			$this->postDate = new \DateTime();
 			return;
 		}
 
-	}
+
 
 	//store the like date using the ValidateDate trait
 	try {
